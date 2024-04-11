e04850dd80ca50ee6177b5df6874992b9418fc4a
PantheraLeo14 <leostark375@gmail.com>
2024-04-12 01:29:29 +0800

    update

--- exercises/tests/tests9.rs
+++ exercises/tests/tests9.rs
@@ -29,22 +29,18 @@
 
 // I AM NOT DONE
 
-
 extern "Rust" {
     fn my_demo_function(a: u32) -> u32;
+
+    #[link_name = "my_demo_function"]
     fn my_demo_function_alias(a: u32) -> u32;
 }
 
-mod foo {
+mod Foo {
     // No `extern` equals `extern "Rust"`.
     #[no_mangle]
     fn my_demo_function(a: u32) -> u32 {
         a
-    }
-
-    #[no_mangle]
-    pub fn my_demo_function_alias(a: u32) -> u32 {
-        my_demo_function(a) // Alias simply forwards the call to the original function
     }
 }
 
