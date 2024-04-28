20732baddd3eec342446da3b0c87d50458d27a0e
PantheraLeo14 <leostark375@gmail.com>
2024-04-28 19:22:45 +0800

    update

--- exercises/macros/macros4.rs
+++ exercises/macros/macros4.rs
@@ -2,17 +2,19 @@
 //
 // Execute `rustlings hint macros4` or use the `hint` watch subcommand for a
 // hint.
+
+
 #[rustfmt::skip]
 macro_rules! my_macro {
     () => {
         println!("Check out my macro!");
-    };
-    ($val:expr) => {
-        println!("Look at this other macro: {}", $val);
     }
+    // ($val:expr) => {
+    //     println!("Look at this other macro: {}", $val);
+    // }
 }
 
 fn main() {
     my_macro!();
-    my_macro!(7777);
+    //my_macro!(7777);
 }
--- exercises/threads/threads3.rs
+++ exercises/threads/threads3.rs
@@ -2,6 +2,9 @@
 //
 // Execute `rustlings hint threads3` or use the `hint` watch subcommand for a
 // hint.
+
+
+
 use std::sync::mpsc;
 use std::sync::Arc;
 use std::thread;
@@ -27,11 +30,12 @@ fn send_tx(q: Queue, tx: mpsc::Sender<u32>) -> () {
     let qc = Arc::new(q);
     let qc1 = Arc::clone(&qc);
     let qc2 = Arc::clone(&qc);
-    let tx1 = tx.clone();
+    let x=tx.clone();
+
     thread::spawn(move || {
         for val in &qc1.first_half {
             println!("sending {:?}", val);
-            tx1.send(*val).unwrap();
+            x.send(*val).unwrap();
             thread::sleep(Duration::from_secs(1));
         }
     });
